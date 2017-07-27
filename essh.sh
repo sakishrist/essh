6e3b9f00f0fd502de32c19167904a3b92f8705ab
Sakis Hrist <sakishrist@gmail.com>
2017-07-27 16:00:15 +0300

    Initial commit

---
+++ essh.sh
@@ -0,0 +1,41 @@
+ESSH_FUNCTIONS="essh bin2int int2bin sshrm ts ts2 injection"
+ESSH_ALIASES="chn gtree srsync wget openports"
+
+injection () {
+	local injection n=$'\n'
+
+	if [[ -n ${ESSH_FUNCTIONS// } ]]; then
+		functions="$(declare -f $ESSH_FUNCTIONS | sed 's/\\/\\\\/g;s/\$/\\$/g;s/"/\\"/g')""$n" # Declare the functions on the remote end
+	fi
+
+	if [[ -n ${ESSH_ALIASES// } ]]; then
+		aliases="$(alias $ESSH_ALIASES | sed 's/\$/\\$/g;s/"/\\"/g')""$n"
+	fi
+
+	if [[ $1 == "-r" ]]; then
+		injection+='exec $SHELL --rcfile '
+	elif [[ $1 == "-l" ]]; then
+		injection+="$SHELL --rcfile "
+	else
+		return 1;
+	fi
+
+	injection+="<("
+	injection+=" cat ~/.bashrc;"
+	injection+=" echo;"
+	injection+=" echo \"ESSH_FUNCTIONS='$ESSH_FUNCTIONS'\";"
+	injection+=" echo \"ESSH_ALIASES='$ESSH_ALIASES'\";"
+	injection+=" echo \"$functions$n$aliases\"; "
+	#injection+=" echo \"PS1='# '\"; "
+	injection+=")"
+	echo "$injection"
+}
+
+
+essh () { #DOC: Like ssh but make available some function you have declared. Note that this will suppress the motd.
+	if [[ -n $@ ]]; then
+		ssh -t "$@" "$(injection -r)"
+	else
+		ssh
+	fi
+}
