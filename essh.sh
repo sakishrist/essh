16a1a0a920a91547add018522cfd8083b0460711
Sakis Hrist <sakishrist@gmail.com>
2017-12-07 17:33:59 +0200

    Add some documentation

--- essh.sh
+++ essh.sh
@@ -14,26 +14,38 @@
 #
 # ESSH_ALIASES="chn gtree srsync wget openports"
 
+# Prepare the script to be executed on the remote system.
 injection () {
 	local injection n=$'\n'
 
+	# Get the function definitions as they are currently known by the running bash.
+	# This means that we do not read from a file and can perform this step again
+	# on the remote host to transfer the functions a second time to the next hop.
 	if [[ -n ${ESSH_FUNCTIONS// } ]]; then
-		functions="$(declare -f $ESSH_FUNCTIONS | sed 's/\\/\\\\/g;s/\$/\\$/g;s/"/\\"/g')""$n" # Declare the functions on the remote end
+		functions="$(declare -f $ESSH_FUNCTIONS | sed 's/\\/\\\\/g;s/\$/\\$/g;s/"/\\"/g')""$n" 
 	fi
 
+	# Get the alias definitions
 	if [[ -n ${ESSH_ALIASES// } ]]; then
 		aliases="$(alias $ESSH_ALIASES | sed 's/\$/\\$/g;s/"/\\"/g')""$n"
 	fi
-
+	
+	# This part tells the remote end to store the definitions of the functions in a variable
+	# This is usefull if the remote end has a bash version older than 4 and has the
+	# pipe sourcing bug.
 	injection+="export init=\"ESSH_FUNCTIONS='$ESSH_FUNCTIONS'"$'\n'
 	injection+="ESSH_ALIASES='$ESSH_ALIASES';"$'\n'
 	injection+="$functions$n$aliases\";"$'\n'
 	
+	# If the version of the remote bash has the pipe sourcing bug, tell the user to manually
+	# run the script in the $init variable and define the function on that end.
 	injection+="if [[ \$BASH_VERSINFO -lt 4 ]]; then"$'\n'
 	injection+="echo 'execute: eval \"\$init\" (quotes are important)'"$'\n'
 	injection+="exec \$SHELL"$'\n'
 	injection+="fi"$'\n'
 	
+	# rcfile option instructs the remote bash to read the initialization script (.bashrc)
+	# from the pipe instead of from the regular location.
 	if [[ $1 == "-r" ]]; then
 		injection+='exec $SHELL --rcfile '
 	elif [[ $1 == "-l" ]]; then
@@ -42,8 +54,11 @@ injection () {
 		return 1;
 	fi
 
+	# Construct the pipe
 	injection+="<("
+	# Read the original bashrc
 	injection+=" cat ~/.bashrc;"
+	# Append the prepared init script
 	injection+=" echo; echo \"\$init\"; "
 	injection+=")"
 	
@@ -53,8 +68,10 @@ injection () {
 
 essh () { #DOC: Like ssh but make available some function you have declared. Note that this will suppress the motd.
 	if [[ -n $@ ]]; then
+		# Start an ssh session and run the commands prepared by injection()
 		ssh -t "$@" "$(injection -r)"
 	else
+		# In case the user did not provide any argument, just run ssh to show the help message.
 		ssh
 	fi
 }
